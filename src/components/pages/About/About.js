dceb9f591b00ee17775d4d679e9b5e8261a2601a
Krystyn Smith <krskrsmith@gmail.com>
2021-12-17 22:06:53 -0600

    added about me

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -3,14 +3,15 @@ import React from 'react';
 export default function About() {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1>About</h1>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+      Evolving web developer with a background in project management and Kaizen. 
+      Currently earning a certificate through Northwestern University in a coding 
+      bootcamp. Strengths in HTML/CSS, JavaScript, Node.js, MySQL, and Agile. 
+      Creative and innovative problem solver who is passionate about technology 
+      accessibility. Has an established history of leading teams in projects 
+      with Lean practices at the core of every task. With each project, keeps 
+      the end user at the forefront of the decisions that are made.
       </p>
     </div>
   );
