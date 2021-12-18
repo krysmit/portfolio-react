6f45a49e8015705c38d0519d84dd039a738204ca
Krystyn Smith <krskrsmith@gmail.com>
2021-12-17 23:24:51 -0600

    added context to the resume page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,17 +5,17 @@ return (
     <div>
         <p>
             <a href='https://github.com/krysmit'>GitHub</a>
-            <img src='./images/github.png' atl='github logo'></img>
+            <img src='/images/github.png' atl='github logo'></img>
         </p>
 
         <p>
         <a href='https://www.linkedin.com/in/krystyn-smith-37477694/'>LinkedIn</a>
-        <img src='./images/linkedin.png' atl='linkedin logo'></img>
+        <img src='/images/linkedin.png' atl='linkedin logo'></img>
         </p>
 
         <p>
         <a href='https://www.instagram.com/earthtokrystyn/?hl=en'>Instagram</a>
-        <img src='./images/instagram.png' atl='instagram logo'></img>
+        <img src='/images/instagram.png' atl='instagram logo'></img>
         </p>
     </div>
 )
--- src/components/pages/Resume/Resume.js
+++ src/components/pages/Resume/Resume.js
@@ -3,10 +3,26 @@ import React from 'react';
 export default function Resume() {
   return (
     <div>
-      <h1>Resume Page</h1>
-      <p>
-        Click here to download my resume.
-      </p>
+      <h1>
+        Click <a href='KrystynSmith-Resume.pdf'>here</a> to download my resume.
+      </h1>
+      <h2>
+        Proficiencies
+      </h2>
+      <ul>
+        <li>HTML/CSS</li>
+        <li>JavaScript</li>
+        <li>Bootstrap</li>
+        <li>Handlebars.js</li>
+        <li>Node.js</li>
+        <li>Nodemailer.js</li>
+        <li>Jest</li>
+        <li>Inquirer</li>
+        <li>JQuery</li>
+        <li>MySQL</li>
+        <li>Sequelize</li>
+        <li>Agile</li>
+      </ul>
     </div>
   );
 }
